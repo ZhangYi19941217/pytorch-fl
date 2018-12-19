817b452db3f71a36a5e1f693d19daca77d5378d9
张翼 <zhangyi19941217@gmail.com>
2018-12-19 12:14:56 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import time
 import torch.nn as nn
+import torch.nn.functional as F
 
 class CNN(nn.Module):
 
@@ -16,53 +17,57 @@ class CNN(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
         )
-        self.out = nn.Linear(in_features=32*7*7, out_features=10)
+        self.out = nn.Sequential(
+            nn.Linear(in_features=32*7*7, out_features=10),
+        )
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = x.view(x.size(0), -1)
+
         output = self.out(x)
-        return output, x
+        return output
 
 
 
-class AlexNet(nn.Module):
+class CNNMnist(nn.Module):
 
     def __init__(self):
-        super(AlexNet, self).__init__()
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=96, kernel_size=3, stride=2, padding=5),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2,stride=2),
-
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3,stride=2),
-
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3,stride=2),
-
-        )
-        self.fc_layers = nn.Sequential(
-            nn.Linear(256*1*1, 4096),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 10),
-        )
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = self.conv_layers(x)
-        x = x.view(x.size(0), -1)
-        output = self.fc_layers(x)
-        return output, x
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)    
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
-    
+
+
+class CNNCifar(nn.Module):
+
+    def __init__(self, args):
+        super(CNNCifar, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, args.num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
