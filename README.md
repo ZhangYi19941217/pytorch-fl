bc685307adfd713c406d1f1b4f429f9520cb6258
chenc10 <chenc10@126.com>
2018-12-19 10:12:33 +0000

    minor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # federated learning in pytorch
+
+
+1. Environmental Variables Required:
+	workers="
+		ip-1
+		ip-2
+		ip-3"
+	master="
+		ip-1"
+
+2. To train in a distributed manner, run:
+	bash train.sh
--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
+echo 'master(coordinator): '$master
 echo 'worker_hosts: '$workers
 num=0
 for i in $workers
 do
-	nohup ssh $i "python /home/ubuntu/yi-fl/single_slave.py --address='172.31.46.217' --rank=$num" >/home/ubuntu/worker_$num.log 2>&1 &
+	nohup ssh $i "python /home/ubuntu/pytorch-fl/single_slave.py --address=$master --rank=$num" >/home/ubuntu/worker_$num.log 2>&1 &
 	num=$((num+1))
 done
