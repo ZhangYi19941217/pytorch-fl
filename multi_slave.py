a1ab5d24a05f55254ce0ababc06857012937f66f
张翼 <zhangyi19941217@gmail.com>
2018-12-19 12:15:16 +0800

    Update multi_slave.py

--- multi_slave.py
+++ multi_slave.py
@@ -1,94 +1,110 @@
 import torch
 import torch.distributed.deprecated as dist
 from datasource import Mnist, Mnist_noniid, Cifar10, Cifar10_noniid
-import model
-import time
+from model import CNNMnist, CNNCifar
 import copy
 from torch.multiprocessing import Process
+import argparse
+import time
 
-MAX_EPOCH = 500
 LR = 0.001
 IID = False
 
-def get_new_model(model, group):
+def get_global_model(model, group):
     for param in model.parameters():
         dist.broadcast(param.data, src=0, group=group)
-    return  model
+    return model
 
-def run(size, rank):
+def get_local_data(rank, batchsize):
+    if IID == True:
+        train_loader = Mnist(batchsize).get_train_data()
+    else:
+        if rank == 0:
+            train_loader = Mnist_noniid(batchsize).get_train_data1()
+        if rank == 1:
+            train_loader = Mnist_noniid(batchsize).get_train_data2()
+        if rank == 2:
+            train_loader = Mnist_noniid(batchsize).get_train_data3()
+        if rank == 3:
+            train_loader = Mnist_noniid(batchsize).get_train_data4()
+        if rank == 4:
+            train_loader = Mnist_noniid(batchsize).get_train_data5()
+    return train_loader
 
-    modell = model.CNN()
+def get_testset(batchsize=100):
+    if IID == True:
+        test_loader = Mnist(batchsize).get_test_data()
+    else:
+        test_loader = Mnist_noniid(batchsize).get_test_data()
+    for step, (b_x, b_y) in enumerate(test_loader):
+        test_x = b_x
+        test_y = b_y
+    return test_x, test_y
 
-    optimizer = torch.optim.Adam(modell.parameters(), lr=LR)
+def init_param(model, src, group):
+    for param in model.parameters():
+        dist.broadcast(param.data, src=src, group=group)
+
+
+def run(size, rank, epoch, batchsize):
+    MAX_EPOCH = epoch
+    model = CNNMnist()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
     loss_func = torch.nn.CrossEntropyLoss()
 
-    if IID == True:
-        train_loader = Mnist().get_train_data()
-        test_loader = Mnist().get_test_data()
-    else:
-        if rank > 0:
-            if rank == 1:
-                train_loader = Mnist_noniid().get_train_data1()
-                test_loader = Mnist_noniid().get_test_data()
-            if rank == 2:
-                train_loader = Mnist_noniid().get_train_data2()
-            if rank == 3:
-                train_loader = Mnist_noniid().get_train_data3()
-            if rank == 4:
-                train_loader = Mnist_noniid().get_train_data4()
-            if rank == 5:
-                train_loader = Mnist_noniid().get_train_data5()
- 
-    if rank == 1:
-        for step, (b_x, b_y) in enumerate(test_loader):
-            test_x = b_x
-            test_y = b_y
+    train_loader = get_local_data(rank, batchsize)
+    if rank == 0 :
+        test_x, test_y = get_testset()
+        fo = open("file"+str(rank)+".txt", 'w')
 
-    group_list = []
-    for i in range(size):
-        group_list.append(i)
+    group_list = [i for i in range(size)]
     group = dist.new_group(group_list)
 
-
     for epoch in range(MAX_EPOCH):
-
-        modell = get_new_model(modell, group)
-
-        if rank == 1:
-            test_output, last_layer = modell(test_x)
+        if epoch == 0:
+            init_param(model, 0, group)
+        
+        if rank == 0:
+            test_output = model(test_x)
             pred_y = torch.max(test_output, 1)[1].data.numpy()
             accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
+            print('Epoch: ', epoch, ' Rank: ', rank, '| test accuracy: %.2f' % accuracy)
+            fo.write(str(epoch) + "    " + str(rank) + "    " + str(accuracy) + "\n")
 
         for step, (b_x, b_y) in enumerate(train_loader):
-
-            output = modell(b_x)[0]
-            loss = loss_func(output, b_y)
             optimizer.zero_grad()
+            output = model(b_x)
+            loss = loss_func(output, b_y)
             loss.backward()   
             optimizer.step()
 
-        for param in modell.parameters():
-            dist.reduce(param.data, dst=0, op=dist.reduce_op.SUM, group=group)
+        for param in model.parameters():
+            dist.all_reduce(param.data, op=dist.reduce_op.SUM, group=group)
+            param.data /= size
+    
+    fo.close()
 
-        if rank == 1:
-            print('Epoch: ', epoch, ' Rank: ', rank, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
-            with open("file"+str(rank)+".txt", 'a') as fo:
-                fo.write(str(epoch) + "    " + str(rank) + "    " + str(accuracy) + "\n")
-
-
-
-                
-def init_processes(size, rank, run):
+def init_processes(size, rank, epoch, batchsize, run):
     dist.init_process_group(backend='tcp', init_method='tcp://127.0.0.1:5000', world_size=size, rank=rank)
-    run(size, rank)
+    run(size, rank, epoch, batchsize)
+
 
 if __name__ == "__main__":
-    size = 6
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', '-s', type=int, default=5)
+    parser.add_argument('--epoch', '-e', type=int, default=500)
+    parser.add_argument('--batchsize', '-b', type=int, default=100)
+    args = parser.parse_args()
+    
+    size = args.size
+    epoch = args.epoch
+    batchsize = args.batchsize
+
     processes = []
-    for rank in range(1, size):
-        p = Process(target=init_processes, args=(size, rank, run))
+    for rank in range(0, size):
+        p = Process(target=init_processes, args=(size, rank, epoch, batchsize, run))
         p.start()
         processes.append(p)
-
     for p in processes:
         p.join()
+
