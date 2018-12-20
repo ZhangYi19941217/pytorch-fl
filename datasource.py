60581c0a99470e68f07edb48e03568c0b39b466b
张翼 <zhangyi19941217@gmail.com>
2018-12-20 13:04:26 +0800

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -22,7 +22,7 @@ class Mnist():
     MAX_NUM_CLASSES_PER_CLIENT = 5
     BATCH_SIZE = 100
 
-    def __init__(self, batchsize):
+    def __init__(self, batchsize=BATCH_SIZE):
         self.train_data = datasets.MNIST(root='./mnist/', train=True, transform=transforms.ToTensor(), download=True)
         self.test_data = datasets.MNIST(root='./mnist/', train=False, transform=transforms.ToTensor())
 
@@ -37,12 +37,13 @@ class Mnist():
 
 
 
+
 class Mnist_noniid():
     IID = False
     MAX_NUM_CLASSES_PER_CLIENT = 5
     BATCH_SIZE = 100
 
-    def __init__(self, batchsize):
+    def __init__(self, batchsize=BATCH_SIZE, total_part=5):
         self.train_data = datasets.MNIST(root='./mnist/', train=True, transform=transforms.ToTensor(), download=True)
         self.test_data = datasets.MNIST(root='./mnist/', train=False, transform=transforms.ToTensor())
 
@@ -52,31 +53,24 @@ class Mnist_noniid():
         idxs_labels = idxs_labels[  :  , idxs_labels[1, :].argsort()]
         idxs = idxs_labels[0, :]
 
-        p1 = idxs[0:12000]
-        p2 = idxs[12000:24000]
-        p3 = idxs[24000:36000]
-        p4 = idxs[36000:48000]
-        p5 = idxs[48000:60000]
+        each_part_size = len(self.train_data) // total_part
 
-        self.train_loader1 = DataLoader(DatasetSplit(self.train_data, p1), batch_size=batchsize, shuffle=True)
-        self.train_loader2 = DataLoader(DatasetSplit(self.train_data, p2), batch_size=batchsize, shuffle=True)
-        self.train_loader3 = DataLoader(DatasetSplit(self.train_data, p3), batch_size=batchsize, shuffle=True)
-        self.train_loader4 = DataLoader(DatasetSplit(self.train_data, p4), batch_size=batchsize, shuffle=True)
-        self.train_loader5 = DataLoader(DatasetSplit(self.train_data, p5), batch_size=batchsize, shuffle=True)
+        partition = []
+        for i in range(total_part):
+            first = each_part_size * i
+            last = each_part_size * (i+1)
+            part = idxs[first:last]
+            partition.append(part)
+        
+        self.train_loader = []
+        for part in partition:
+            loader = DataLoader(DatasetSplit(self.train_data, part), batch_size=batchsize, shuffle=True)
+            self.train_loader.append(loader)
 
         self.test_loader = torch.utils.data.DataLoader(dataset=self.test_data, batch_size=10000, shuffle=True)
 
-
-    def get_train_data1(self):  
-        return self.train_loader1
-    def get_train_data2(self):
-        return self.train_loader2
-    def get_train_data3(self):
-        return self.train_loader3
-    def get_train_data4(self):
-        return self.train_loader4
-    def get_train_data5(self):
-        return self.train_loader5
+    def get_train_data(self, part_id):
+        return self.train_loader[part_id]
 
     def get_test_data(self):
         return self.test_loader
@@ -88,7 +82,7 @@ class Cifar10():
     MAX_NUM_CLASSES_PER_CLIENT = 5
     BATCH_SIZE = 100
 
-    def __init__(self, batchsize):
+    def __init__(self, batchsize=BATCH_SIZE):
 
         self.train_data = datasets.CIFAR10(root='./cifar10/', train=True, transform=transforms.ToTensor(), download=True)
         self.test_data = datasets.CIFAR10(root='./cifar10/', train=False, transform=transforms.ToTensor())
@@ -101,50 +95,42 @@ class Cifar10():
 
     def get_test_data(self):
         return self.test_loader
+    
 
 
-
-
-class Cifar10_noniid():
+class Cifar10_noniid(): 
     IID = False
     MAX_NUM_CLASSES_PER_CLIENT = 5
     BATCH_SIZE = 100
 
-    def __init__(self, batchsize):
+    def __init__(self, batchsize=BATCH_SIZE, total_part=5):
         self.train_data = datasets.CIFAR10(root='./cifar10/', train=True, transform=transforms.ToTensor(), download=True)
         self.test_data = datasets.CIFAR10(root='./cifar10/', train=False, transform=transforms.ToTensor())
 
         idxs = np.arange(len(self.train_data))
-        labels = np.array(self.train_data.train_labels)
+        labels = self.train_data.train_labels.numpy()
         idxs_labels = np.vstack((idxs, labels))
         idxs_labels = idxs_labels[  :  , idxs_labels[1, :].argsort()]
         idxs = idxs_labels[0, :]
 
-        p1 = idxs[0:10000]
-        p2 = idxs[10000:20000]
-        p3 = idxs[20000:30000]
-        p4 = idxs[30000:40000]
-        p5 = idxs[40000:50000]
+        each_part_size = len(self.train_data) // total_part
 
-        self.train_loader1 = DataLoader(DatasetSplit(self.train_data, p1), batch_size=batchsize, shuffle=True)
-        self.train_loader2 = DataLoader(DatasetSplit(self.train_data, p2), batch_size=batchsize, shuffle=True)
-        self.train_loader3 = DataLoader(DatasetSplit(self.train_data, p3), batch_size=batchsize, shuffle=True)
-        self.train_loader4 = DataLoader(DatasetSplit(self.train_data, p4), batch_size=batchsize, shuffle=True)
-        self.train_loader5 = DataLoader(DatasetSplit(self.train_data, p5), batch_size=batchsize, shuffle=True)
+        partition = []
+        for i in range(total_part):
+            first = each_part_size * i
+            last = each_part_size * (i+1)
+            part = idxs[first:last]
+            partition.append(part)
+        
+        self.train_loader = []
+        for part in partition:
+            loader = DataLoader(DatasetSplit(self.train_data, part), batch_size=batchsize, shuffle=True)
+            self.train_loader.append(loader)
 
         self.test_loader = torch.utils.data.DataLoader(dataset=self.test_data, batch_size=10000, shuffle=True)
 
-
-    def get_train_data1(self):  
-        return self.train_loader1
-    def get_train_data2(self):
-        return self.train_loader2
-    def get_train_data3(self):
-        return self.train_loader3
-    def get_train_data4(self):
-        return self.train_loader4
-    def get_train_data5(self):
-        return self.train_loader5
+    def get_train_data(self, part_id):
+        return self.train_loader[part_id]
 
     def get_test_data(self):
         return self.test_loader
