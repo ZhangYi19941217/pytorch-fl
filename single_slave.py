c14172048ce696152f8f5944586ca7705da7a95e
张翼 <zhangyi19941217@gmail.com>
2018-12-21 15:21:37 +0800

    Update single_slave.py

--- single_slave.py
+++ single_slave.py
@@ -1,19 +1,24 @@
 import torch
 import torch.distributed.deprecated as dist
 from datasource import Mnist, Mnist_noniid, Cifar10, Cifar10_noniid
-from model import CNNMnist, CNNCifar
+from model import CNNMnist, CNNCifar, ResNet18
 import copy
 from torch.multiprocessing import Process
 import argparse
 import time
 import sys
+import os
 sys.stdout.flush()
 
 LR = 0.001
+MAX_ROUND = 3000
+ROUND_NUMBER_FOR_SAVE = 10
+ROUND_NUMBER_FOR_REDUCE = 5
 IID = False
 DATA_SET = 'Mnist'
 #DATA_SET = 'Cifar10'
 MODEL = 'CNN'
+#MODEL = 'ResNet18'
 
 def get_local_data(size, rank, batchsize):
     if IID == True:
@@ -46,59 +51,97 @@ def get_testset():
 
 def init_param(model, src, group):
     for param in model.parameters():
-        print(param)
+        #print(param)
         sys.stdout.flush()
         dist.broadcast(param.data, src=src, group=group)
-        print('done')
+        #print('done')
         sys.stdout.flush()
+    
+def save_model(model, round, rank):
+    print('===> Saving models...')
+    state = {
+        'state': model.state_dict(),
+        'round': round,
+        }
+    torch.save(state, 'autoencoder' + str(rank) + '.t7')
+
+def load_model(model, group, rank):
+    print('===> Try resume from checkpoint')
+    if os.path.exists('autoencoder' + str(rank) + '.t7'):
+        checkpoint = torch.load('autoencoder' + str(rank) + '.t7')
+        model.load_state_dict(checkpoint['state'])
+        round = checkpoint['round']
+        print('===> Load last checkpoint data')
+    else:
+        round = 0
+        init_param(model, 0, group)
+    return model, round
+
+
+def all_reduce(model, size, group):
+    for param in model.parameters():
+        dist.all_reduce(param.data, op=dist.reduce_op.SUM, group=group)
+    param.data /= size
+    return model
+
+def exchange(model, size, rank):
+    old_model = copy.deepcopy(model)
+    for param in old_model.parameters():
+        dist.isend( param.data, dst=(rank+1)%size )
+    for param in model.parameters():
+        dist.recv( param.data, src=(rank-1)%size )
+    return model
+
 
 def run(size, rank, epoch, batchsize):
-    print('run')
-    MAX_EPOCH = epoch
+    #print('run')
     if MODEL == 'CNN' and DATA_SET == 'Mnist':
         model = CNNMnist()
     if MODEL == 'CNN' and DATA_SET == 'Cifar10':
         model = CNNCifar()
+    if MODEL == 'ResNet18' and DATA_SET == 'Cifar10':
+        model = ResNet18()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR, weight_decay=1e-5)
     loss_func = torch.nn.CrossEntropyLoss()
 
     train_loader = get_local_data(size, rank, batchsize)
     if rank == 0 :
         test_x, test_y = get_testset()
-        #fo = open("file"+str(rank)+".txt", 'w')
+        #fo = open("file_multi"+str(rank)+".txt", 'w')
 
     group_list = [i for i in range(size)]
     group = dist.new_group(group_list)
 
-    for epoch in range(MAX_EPOCH):
-        print('enter epoch: ' + str(epoch))
+    model, round = load_model(model, group, rank)
+    while round < MAX_ROUND:
         sys.stdout.flush()
-        if epoch == 0:
-            init_param(model, 0, group)
-        
         if rank == 0:
             test_output = model(test_x)
             pred_y = torch.max(test_output, 1)[1].data.numpy()
             accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
-            print('Epoch: ', epoch, ' Rank: ', rank, '| test accuracy: %.2f' % accuracy)
-            #fo.write(str(epoch) + "    " + str(rank) + "    " + str(accuracy) + "\n")
+            print('Round: ', round, ' Rank: ', rank, '| test accuracy: %.2f' % accuracy)
+            #fo.write(str(round) + "    " + str(rank) + "    " + str(accuracy) + "\n")
 
-        for step, (b_x, b_y) in enumerate(train_loader):
-            optimizer.zero_grad()
-            output = model(b_x)
-            loss = loss_func(output, b_y)
-            loss.backward()   
-            optimizer.step()
+        for epoch_cnt in range(epoch):
+            for step, (b_x, b_y) in enumerate(train_loader):
+                optimizer.zero_grad()
+                output = model(b_x)
+                loss = loss_func(output, b_y)
+                loss.backward()   
+                optimizer.step()
 
-        for param in model.parameters():
-            dist.all_reduce(param.data, op=dist.reduce_op.SUM, group=group)
-            param.data /= size
-    
+        model = exchange(model, size, rank)
+        if (round+1) % ROUND_NUMBER_FOR_REDUCE == 0:
+            model = all_reduce(model, size, group)
+
+        if (round+1) % ROUND_NUMBER_FOR_SAVE == 0:
+            save_model(model, round+1, rank)
+        round += 1
     #fo.close()
 
 def init_processes(address, port, size, rank, epoch, batchsize, run):
-    print('enter init_process')
+    #print('enter init_process')
     address = 'tcp://' + address + ':' + str(port)
     dist.init_process_group(backend='tcp', init_method=address, world_size=size, rank=rank)
     run(size, rank, epoch, batchsize)
@@ -110,7 +153,7 @@ if __name__ == "__main__":
     parser.add_argument('--port', '-p', type=int, default=22222)
     parser.add_argument('--size', '-s', type=int, default=5)
     parser.add_argument('--rank', '-r', type=int, default=0)
-    parser.add_argument('--epoch', '-e', type=int, default=2)
+    parser.add_argument('--epoch', '-e', type=int, default=1)
     parser.add_argument('--batchsize', '-b', type=int, default=100)
     args = parser.parse_args()
     
