eeed57216b7cdf9b35b3d8a176a64082f090767c
chenc10 <chenc10@126.com>
2019-01-18 11:48:28 +0000

    create worker_process_deep_dive

---
+++ train_deep_dive.sh
@@ -0,0 +1,15 @@
+echo 'master(coordinator): '$master
+echo 'worker_hosts: '$workers
+
+world_size=0
+for i in $workers
+do
+		world_size=$((world_size+1))
+done
+
+num=0
+for i in $workers
+do
+	nohup ssh $i "python /home/ubuntu/pytorch-fl/worker_process_deep_dive.py --master_address='tcp://'$master':22222' --rank=$num --world_size=$world_size" >/home/ubuntu/worker_$num.log 2>&1 &
+	num=$((num+1))
+done
