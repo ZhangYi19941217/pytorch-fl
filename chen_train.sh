072bd9ebd17d0739ea3d9f2e347d16be801c728a
chenc10 <chenc10@126.com>
2019-04-12 15:37:55 +0800

    initial version for adaptive frozen

--- chen_train.sh
+++ chen_train.sh
@@ -15,7 +15,7 @@ num=0
 for i in $workers
 do
 	echo "python /Users/chen/Dropbox/NIPS19/code/pytorch-fl/worker_process_3_new.py --master_address='tcp://'$master':22222' --rank=$num --world_size=$world_size --model=$model --dataset=$dataset --iid=$iid"
-	nohup ssh $i "python /Users/chen/Dropbox/NIPS19/code/pytorch-fl/worker_process_3_new.py --master_address='tcp://'$master':22222' --rank=$num --world_size=$world_size --model=$model --dataset=$dataset --iid=$iid" > /Users/chen/Dropbox/NIPS19/code/Log/worker_$num.log 2>&1 &
+	nohup ssh $i "python /Users/chen/Dropbox/NIPS19/code/pytorch-fl/worker_process_adaptive_frozen.py --master_address='tcp://'$master':22222' --rank=$num --world_size=$world_size --model=$model --dataset=$dataset --iid=$iid" > /Users/chen/Dropbox/NIPS19/code/Log/worker_$num.log 2>&1 &
 	num=$((num+1))
 done
 
